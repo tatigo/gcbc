6f623a00083da8176a139cdd1de09c9542a79e47
Tatiana Golod <tgolod@lixar.com>
2018-04-18 15:53:57 -0400

    added network

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,5 +2,7 @@ var Company = artifacts.require("./Company.sol");
 
 module.exports = function(deployer, network, accounts) {
   //deployer.deploy(Company);
-  deployer.deploy(Company, "PizzaPizza", "863049020", null,{gas: 4612388, from: accounts[0]});
+  // deployer.deploy(Company, "PizzaPizza", "863049020", null,{gas: 500000, from: accounts[0]});
+  deployer.deploy(Company, "PizzaPizza", "863049020", null);
+  
 };
--- truffle.js
+++ truffle.js
@@ -11,6 +11,13 @@ module.exports = {
       network_id: '*'
       //,from:'0x82300f77f7f8a925f3156294b2cad52506cca52c' //default account is [0] if not specified
       //,gas:4712387
+    },
+    remotetg:{
+      host: "ethrus-dns-reg1.eastus.cloudapp.azure.com",
+      port: 8545,
+      network_id: '*',
+      gas:500000
+      //,from:'0x82300f77f7f8a925f3156294b2cad52506cca52c' //default account is [0] if not specified
     }
   }
 };
