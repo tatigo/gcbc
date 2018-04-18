6985f13662e11975f194e5dad63097c533d59237
Tatiana Golod <tgolod@lixar.com>
2018-04-18 11:46:03 -0400

    added  zeppelin-solidity libraries modfied ElectronocRecord.sol to be Business.sol

--- truffle.js
+++ truffle.js
@@ -4,6 +4,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    remote:{
+      host: "52.170.24.21",
+      port: 22,
+      network_id: '*'
+      //,from:'0x82300f77f7f8a925f3156294b2cad52506cca52c' //default account is [0] if not specified
+      //,gas:4712387
     }
   }
 };
